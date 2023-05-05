b503f08261a6a5eec1843d2c2691f28ba030865f
Paweł Kołaczyński <kolaczyn@protonmail.com>
2023-05-05 19:37:20 +0200

    setup links

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,27 +1,27 @@
 type Color = string;
 
-type GitHubLink = {
+export type GitHubLink = {
   type: "link/github";
   url: `https://github.com/${string}`;
 };
 
-type MalLink = {
+export type MalLink = {
   type: "link/mal";
   url: `https://myanimelist.net/profile/${string}`;
 };
 
-type TwitterLink = {
+export type TwitterLink = {
   type: "link/twitter";
   url: `https://twitter.com/${string}`;
 };
 
-type CustomLink = {
+export type CustomLink = {
   type: "link/custom";
   url: string;
   label: string;
 };
 
-type AppLink = GitHubLink | MalLink | TwitterLink | CustomLink;
+export type AppLink = GitHubLink | MalLink | TwitterLink | CustomLink;
 
 type Background = BgGradient | BgSolid;
 
